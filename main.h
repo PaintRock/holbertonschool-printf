ab0f39e76700ea652505aeb069d4fd1a8dad8f38
adel <6126.holbertonstudents.com>
2023-03-09 09:56:42 -0800

    prototype and structs added to main_h

--- main.h
+++ main.h
@@ -3,12 +3,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
+#include <limits.h>
+#include <unistd.h>
+
+typedef struct functions
+{
+	char *poop;
+	void(*f)(char*, va_list, int*);
+}functions;
 
 
-
-
-
-
+/* Prototypes */
+int (_printf(const char *format, ...);
+     void(get_op_functions(char c))(char*, va_list, int*);
+     void op_string(char *buffer, va_list args, int *buf_count);
+     void op_char(char *buffer, va_list args, int *buf_count);
+     void op_int(char *buffer, va_list args, int *buf_count);
+     void op_percent(char *buffer, va_list args, int *buf_count);
+#endif
 
 
 
