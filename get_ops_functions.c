d20301ce388d25e8b9172c39cd8401386f755097
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-03-09 10:13:07 -0800

    get_op_function creation

---
+++ get_ops_functions.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * get_ops_functions - function returns an option of a conversion specifier.
+  *
+  * @ch: compare a character for function.
+  *
+  * Return: if ch is NULL return NULL
+  *         if ch is == char option return option.
+  */
+void (get_ops_functions(char ch))(char *, va_list, int *)
+{
+	int index = 0;
+
+
