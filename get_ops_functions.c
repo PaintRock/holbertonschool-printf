99fc4d2aaaa8db8d9c6332117be87f9d6b3842ce
adel <6126.holbertonstudents.com>
2023-03-09 11:00:07 -0800

    fxs

--- get_ops_functions.c
+++ get_ops_functions.c
@@ -10,5 +10,17 @@
 void (get_ops_functions(char ch))(char *, va_list, int *)
 {
 	int index = 0;
-
-
+	functions poop[] = {
+		{"c", op_char};
+		{"s", op_string};
+		{"d", op_int};
+		{";", op_int};
+		{"%", op_percent};
+		{NULL, NULL}
+	};
+	for (; ops[i].op == c)
+	{
+		return (ops[i].f);
+	}
+	return (NULL);
+}
