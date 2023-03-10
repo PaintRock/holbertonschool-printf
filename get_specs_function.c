4be235f31079c85fefbead51e8077ee91a0f080c
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-03-10 08:57:03 -0800

    specifier functions source file added updates and edits to get_ops now spec ops

--- get_specs_function.c
+++ get_specs_function.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
-  * get_ops_functions - function returns an option of a conversion specifier.
+  * get_spec_functions - function returns an option of a conversion specifier.
   *
   * @ch: compare a character for function.
   *
   * Return: if ch is NULL return NULL
   *         if ch is == char option return option.
   */
-void (get_ops_functions(char ch))(char *, va_list, int *)
+void *(get_spec_functions(char ch))(char *, va_list, int *)
 {
-	int index = 0;
+	int i = 0;
 	functions poop[] = {
 		{"c", op_char},
 		{"s", op_string},
@@ -18,9 +18,13 @@ void (get_ops_functions(char ch))(char *, va_list, int *)
 		{"%", op_percent},
 		{NULL, NULL}
 	};
-	for (; poop[i].op == c)
+	for (; poop[i].poop != NULL && *(poop[i].poop) != ch; i++)
 	{
-		return (poop[i].f);
+		/* do nothing */
 	}
-	return (NULL);
+	if (poop[i].poop == NULL)
+	{
+		return (NULL);
+	}
+	return (poop[i].f);
 }
