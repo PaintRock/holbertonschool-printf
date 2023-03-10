ab0f26aedde4fbcc6035121ad7ffd7c468886028
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-03-10 12:01:41 -0800

    edit _printf; modified main.h; fixed typos spec_functions; modified get_specs to exclude %

---
+++ _printf.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _printf - recreation of printf to find certain specifiers
+ * Description: Funtion that prints string format.
+ *
+ * @format: arguments function
+ * Return: an int len of string to '\0'
+ */
+
+int _printf(const char *format, ...)
+{
+	int i = 0, bf_count = 0;
+	char *buffer = NULL;
+	void (*spec_functions)(char *, va_list, int *);
+	va_list args;
+
+	va_start(args, format), buffer = malloc(sizeof(char) * 2048);
+	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+		return (-1);
+	if (!format[i])
+		return (0);
+	for (; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+				continue;
+
+			spec_functions = get_spec_functions(format[i + 1]);
+			if (spec_functions == NULL)
+			{
+				buffer[bf_count] = format[i];
+
+				if (format[i + 1] != '%')
+				{
+					buffer[bf_count + 1] = format[i + 1];
+					bf_count += 2;
+				}
+				else
+					bf_count++;
+			}
+			else
+				spec_functions(buffer, args, &bf_count);
+			i++;
+			continue;
+		}
+		buffer[bf_count] = format[i];
+		bf_count++;
+	}
+	va_end(args), write(1, buffer, bf_count), free(buffer);
+	return (bf_count);
+}
--- spec_functions.c
+++ spec_functions.c
@@ -18,11 +18,11 @@
 
 void spec_char(char *buffer, va_list args, int *bf_count)
 {
-        char c;
+	char c;
 
-        c = (char)va_arg(args, int);
-        buffer[*bf_count] = c;
-        (*bf_count)++;
+	c = (char)va_arg(args, int);
+	buffer[*bf_count] = c;
+	(*bf_count)++;
 }
 
 /**
@@ -43,19 +43,18 @@ void spec_char(char *buffer, va_list args, int *bf_count)
 
 void spec_string(char *buffer, va_list args, int *bf_count)
 {
-        int i;
-        char *ch = NULL;
+	int i;
+	char *ch = NULL;
 
-        ch = va_arg(args, char *);
+	ch = va_arg(args, char *);
 
-        if (ch == NULL)
-                ch = "(null)";
-
-        for (i = 0; ch[i] != '\0'; i++)
-        {
-                buffer[*bf_count] = ch[i];
-                (*bf_count)++;
-        }
+	if (ch == NULL)
+		ch = "(null)";
+	for (i = 0; ch[i] != '\0'; i++)
+	{
+		buffer[*bf_count] = ch[i];
+		(*bf_count)++;
+	}
 }
 /**
 * spec_int - handle specific char 'i' and 'd'
@@ -74,63 +73,41 @@ void spec_string(char *buffer, va_list args, int *bf_count)
 */
 void spec_int(char *buffer, va_list args, int *bf_count)
 {
-        int arg, temp_arg, temp_num, num = 0, digits[15];
+	int arg, temp_arg, temp_num, num = 0, digits[15];
 
-        args = temp_arg = (int)va_arg(args, int);
-
-        if (arg == 0)
-        {
-                buffer[*bf_count] = 48;
-                (*bf_count)++;
-        }
-        if (arg > 0)
-        {
-                while (arg != 0)
-                {
-                        temp_arg = arg % 10;
-                        arg /= 10;
-                        digits[num] = temp_arg;
-                        num++;
-                }
-        }
-        else if (arg < 0)
-        {
-                buffer[*bf_count] = '-';
-                (*bf_count)++;
-                while (arg != 0)
-                {
-                        temp_num = arg % 10 * -1;
-                        digits[num] = temp_num;
-                        arg /= 10;
-                        num++;
-                }
-        }
-        num--;
-        while (num >= 0)
-        {
-                buffer[*bf_count] = digits[num] + '0';
-                (*bf_count)++;
-                num--;
-        }
-}
-
-/**
-  * spec_percent- handle specific char '%'
-  * from list of args
-  *
-  * Description: Function that returns a character.
-  *
-  * @buffer: points to memory location
-  * where output is stored
-  *
-  * @args: Arguments passed to printf function
-  *
-  * @bf_count: Iterating pointer to an int that
-  * keeps track of current position of the buffer.
-  *
-  */
-void spec_percent(char *buffer, va_list args, int *bf_count)
-{
-        buffer[*bf_count] = '%';
-        (*bf_count)++;
+	arg = temp_arg = (int)va_arg(args, int);
+	if (arg == 0)
+	{
+		buffer[*bf_count] = 48;
+		(*bf_count)++;
+	}
+	if (arg > 0)
+	{
+		while (arg != 0)
+		{
+			temp_arg = arg % 10;
+			arg /= 10;
+			digits[num] = temp_arg;
+			num++;
+		}
+	}
+	else if (arg < 0)
+	{
+		buffer[*bf_count] = '-';
+		(*bf_count)++;
+		while (arg != 0)
+		{
+			temp_num = arg % 10 * -1;
+			digits[num] = temp_num;
+			arg /= 10;
+			num++;
+		}
+	}
+	num--;
+	while (num >= 0)
+	{
+		buffer[*bf_count] = digits[num] + '0';
+		(*bf_count)++;
+		num--;
+	}
 }
